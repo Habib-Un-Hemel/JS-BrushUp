1f4a9762cac96c7ac170a42ae2a5665109c9e011
Habibun Nabi Hemel <150641939+Habib-Un-Hemel@users.noreply.github.com>
2024-06-14 10:18:07 +0000

    new changes

--- 01_Basics/03_typeConversion.js
+++ 01_Basics/03_typeConversion.js
@@ -1,5 +1,95 @@
+let score = "Hemel"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Hemel"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Hemel" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
 /*
 const score = 10;
 we know that its a number but sometime we will get the value from a form where we need to check the types
  
+*/
+
+let score = 10;
+console.log(typeof score);
+console.log(typeof(score)); // I can use the typeof as a method
+
+
+let score2 = "10" ;
+let valueInNumebr = Number(score2);
+console.log(typeof valueInNumebr);
+console.log(valueInNumebr)
+
+
+let score3= "10abc" ;
+let valueInNumebr2 = Number(score3);
+console.log(typeof valueInNumebr2);
+console.log(valueInNumebr2) // NaN -> not a number NaN is also a type
+/*
+dont believe in these number || there is no  scirpt check in js || that why people use typeScript 
+form e kaj korle ,browseer normally string dei jeigulare amr change kora lagbe boolean e ,
 */
